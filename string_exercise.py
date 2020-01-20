a19960992433f2d51d55911dd5ff7a933de7fd00
bdcolosi <58956391+bdcolosi@users.noreply.github.com>
2020-01-19 22:54:21 -0500

    Add files via upload

---
+++ string_exercise.py
@@ -0,0 +1,60 @@
+# cats = "My name is Winnie and I am a cat."
+# print(cats.upper())
+
+# meow ="shadow shadow shadow shadow"
+# print(meow.capitalize())
+
+# my_cat = "My name is Onyx and I am anxious." [::-1]
+# print(my_cat)
+
+# user_translate = input("Enter text to translate: ").upper()
+# i = 0
+# new_user_translate = ""
+# while i < len(user_translate):
+#     if user_translate[i] == "A":
+#         new_user_translate += "4"
+#     if user_translate[i] == "E":
+#         new_user_translate += "3"
+#     if user_translate[i] == "G":
+#         new_user_translate += "6"
+#     if user_translate[i] == "I":
+#         new_user_translate += "1"
+#     if user_translate[i] == "O":
+#         new_user_translate += "0"
+#     if user_translate[i] == "S":
+#         new_user_translate += "5"
+#     if user_translate[i] == "T":
+#         new_user_translate += "7"
+#     else:
+#         new_user_translate += user_translate[i]
+#     i += 1
+# print(new_user_translate)
+
+# your_input = input("Enter text ")
+
+# vowels = "aeiou"
+
+# new_your_input = " "
+
+# i = 0 
+
+# while i < len(your_input):
+#     if your_input[i] == your_input[i - 1] and your_input[i] in vowels:
+#         new_your_input += your_input[i] * 4 
+#     else:
+#         new_your_input += your_input[i]
+#     i += 1
+# print(new_your_input)
+        
+secret = input("Translate text here: ")
+new_secret = " "
+i = 0 
+
+while i < len(secret):
+    if secret[i] == [0]:
+        new_secret += 1
+
+
+
+
+
