0970073ad58ca35dd6756f2be54f8e2ebbc9b962
bdcolosi <58956391+bdcolosi@users.noreply.github.com>
2020-01-19 22:52:05 -0500

    Add files via upload

---
+++ list_exercises.py
@@ -0,0 +1,87 @@
+
+# num_list = [27, 4, 10, 30, 24]
+
+# range (27, 100) # list(range(27 , 100))
+
+# print(sum(num_list))
+
+# num = 0 
+# for i in num_list:
+#     num += i
+# print(num)
+
+#------------------------------------
+
+# print(max(num_list))
+
+
+# print(len(num_list))
+
+# print(range(len(num_list)))
+
+#------------------------------------
+
+# print(min(num_list))
+
+#------------------------------------
+
+# even = 0 
+
+# while even < len(num_list):
+#     if even % 2 == 0:
+#         print(num_list[even])
+#     even += 1
+
+# for i in num_list:
+#     if i % 2 == 0:
+#         print(i)
+
+
+
+#------------------------------------
+
+# odd_num = 0 
+
+# while(odd_num < len(num_list)):
+#     if odd_num % 2 != 0:
+#         print(odd_num)
+#     odd_num += 1
+
+
+# for i in num_list:
+#     if i % 2 != 0: 
+#         print(i)
+    
+#------------------------------------
+
+# b = [-1, 5, -67, 22, 19, -200]
+
+# for i in b: 
+#     if i < 0:
+#         print(i)
+
+# for i in b:
+#     if i > 0:
+#         print(i)
+
+#------------------------------------
+
+# q = 30
+
+# def multi(b):
+#     for i in b[0:6]:
+#         print(i * q)
+
+# multi(b)
+
+#-------------------------------------
+
+# a = [2, 10, 5]
+# b = [3, 5, 8]
+
+# def multi2(): 
+#     for i in a[0:3]:
+#         for x in b[0:3]:
+#             print(x*i)
+
+# multi2()
